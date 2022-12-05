67b0cd564fdc95533beae9a41405bfa5368127d7
aditya patil <Aditya@example.com>
2022-12-05 09:33:28 +0530

    [Aditya]Add:Flipping The Coin

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            while (true)
+            {
+                Console.WriteLine("1.FlipCoin");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.CheckHeadTail();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        
+    }
+}
